eefdf955879c791735cb7b848e415382ec0c84e6
Vincent CHALAMON <vincentchalamon@gmail.com>
2011-06-22 14:04:31 +0200

    Suppression des assets

--- web/sfEPFactoryFormPlugin/switcher/switcher.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-  $('.switch label').live('click', function(){
-    if(!$(this).hasClass('selected')) {
-      $(this).addClass('selected').siblings('label').removeClass('selected');
-    }
-  });
-});
