d177b5c02982ccf101e12ac3f33b6d98639387a4
Criels <SocieteCriels@gmail.com>
2011-10-04 11:23:18 +0300

    Correction sur clos

--- apps/frontend/modules/fiche/templates/_show_header.php
+++ apps/frontend/modules/fiche/templates/_show_header.php
@@ -5,6 +5,6 @@
     <p><?php echo $fiche->getResolvedAuthor() ?> a fermé(e) cette fiche le <?php echo format_date(strtotime($fiche->getResolvedDate()), "EEEE dd MMMM yyyy", "fr") ?> à <?php echo date('H\hi', strtotime($fiche->getResolvedDate())) ?></p>
   <?php endif ?>
   <?php if($fiche->getIsFinished()): ?>
-    <p><?php echo $fiche->getFinishedAuthor() ?> a clo(e) cette fiche le <?php echo format_date(strtotime($fiche->getFinishedDate()), "EEEE dd MMMM yyyy", "fr") ?> à <?php echo date('H\hi', strtotime($fiche->getFinishedDate())) ?></p>
+    <p><?php echo $fiche->getFinishedAuthor() ?> a clos cette fiche le <?php echo format_date(strtotime($fiche->getFinishedDate()), "EEEE dd MMMM yyyy", "fr") ?> à <?php echo date('H\hi', strtotime($fiche->getFinishedDate())) ?></p>
   <?php endif ?>
 </div>
